2e875835af54d3d9b37b9eaae12b065d113def43
Paul Seal <prjseal@gmail.com>
2019-04-09 14:03:48 +0100

    - works really well with the infinite editor now

---
+++ GiphyPropertyEditor/App_Plugins/GiphyPropertyEditor/js/picker.controller.js
@@ -0,0 +1,32 @@
+﻿function giphyPickerEditorController($scope, $http) {
+
+    $scope.submit = function (result) {
+        if ($scope.model.submit) {
+            $scope.model.submit(result);
+        }
+    };
+
+    $scope.close = function () {
+        if ($scope.model.close) {
+            $scope.model.close();
+        }
+    };
+
+    $scope.search = function () {
+        $http({
+            method: 'GET',
+            url: 'https://api.giphy.com/v1/gifs/search?api_key=' + $scope.model.apikey + '&q=' + $scope.model.search,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (response) {
+            if (response !== null && response !== undefined && response.data !== undefined) {
+                $scope.model.results = response.data.data;
+            }
+            else {
+                $scope.model.results = null;
+            }
+        });
+    };
+}
+angular.module('umbraco').controller("giphyPickerEditorController", giphyPickerEditorController);
---
+++ GiphyPropertyEditor/App_Plugins/GiphyPropertyEditor/js/propertyeditor.controller.js
@@ -0,0 +1,24 @@
+﻿function giphyPropertyEditorController($scope, editorService) {
+
+    $scope.add = function () {
+        var giphyPicker = {
+            apikey: $scope.model.config.apikey,
+            title: "Giphy Property Editor",
+            view: "/App_Plugins/GiphyPropertyEditor/giphypicker.html",
+            size: "small",
+            submit: function (model) {
+                $scope.model.value = model.images.original.url;
+                editorService.close();
+            },
+            close: function () {
+                editorService.close();
+            }
+        };
+        editorService.open(giphyPicker);
+    };
+
+    $scope.remove = function () {
+        $scope.model.value = null;
+    };
+}
+angular.module('umbraco').controller("giphyPropertyEditorController", giphyPropertyEditorController);
