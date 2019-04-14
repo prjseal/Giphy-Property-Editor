0523d00f0fbb6705b12bf90c6ff56625d107cf97
Paul Seal <prjseal@gmail.com>
2019-04-15 00:02:06 +0100

    - added grid editor - added property value converter

---
+++ GiphyPropertyEditor/App_Plugins/GiphyPropertyEditor/js/giphy.grideditor.controller.js
@@ -0,0 +1,24 @@
+﻿function giphyGridEditorController($scope, editorService) {
+
+    $scope.add = function () {
+        var giphyPicker = {
+            apikey: "59RQWmqS3CsdVbgjd6XfrnUOA8UvbcPY",
+            title: "Animated Gif Search",
+            view: "/App_Plugins/GiphyPropertyEditor/giphy.picker.html",
+            size: "small",
+            submit: function (model) {
+                $scope.control.value = model.images.original.url;
+                editorService.close();
+            },
+            close: function () {
+                editorService.close();
+            }
+        };
+        editorService.open(giphyPicker);
+    };
+
+    $scope.remove = function () {
+        $scope.control.value = null;
+    };
+}
+angular.module('umbraco').controller("Giphy.GridEditor.Controller", giphyGridEditorController);
---
+++ GiphyPropertyEditor/PropertyValueConverters/GiphyPropertyValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace GiphyPropertyEditor.PropertyValueConverters
+{
+
+    public class GiphyPropertyValueConverter : PropertyValueConverterBase
+    {
+
+        /// <summary>
+        /// Gets a value indicating whether the converter supports a property type.
+        /// </summary>
+        /// <param name="propertyType">The property type.</param>
+        /// <returns>A value indicating whether the converter supports a property type.</returns>
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.EditorAlias == "GiphyPropertyEditor";
+        }
+
+        /// <summary>
+        /// Gets the type of values returned by the converter.
+        /// </summary>
+        /// <param name="propertyType">The property type.</param>
+        /// <returns>The CLR type of values returned by the converter.</returns>
+        public override Type GetPropertyValueType(PublishedPropertyType propertyType)
+        {
+            return typeof(string);
+        }
+
+    }
+
+}
