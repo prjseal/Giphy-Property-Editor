a3e1f5fccfb8b0a32ba2f8450ac0afea3edbbc11
Paul Seal <prjseal@gmail.com>
2019-05-02 13:39:43 +0100

    - got property editor working

--- GiphyPropertyEditorUmbraco7/App_Plugins/GiphyPropertyEditorUmbraco7/js/giphy.grideditor.controller.js
+++ GiphyPropertyEditorUmbraco7/App_Plugins/GiphyPropertyEditorUmbraco7/js/giphy.grideditor.controller.js
@@ -1,20 +1,21 @@
 ﻿function giphyGridEditorController($scope, editorService) {
 
-    $scope.add = function () {
-        var giphyPicker = {
-            apikey: Umbraco.Sys.ServerVariables["Giphy"]["ApiKey"],
-            title: "Animated Gif Search",
-            view: "/App_Plugins/GiphyPropertyEditorUmbraco7/giphy.picker.html",
-            size: "small",
-            submit: function (model) {
-                $scope.control.value = model.images.original.url;
-                editorService.close();
-            },
-            close: function () {
-                editorService.close();
-            }
-        };
-        editorService.open(giphyPicker);
+    $scope.overlay = {
+        apikey: Umbraco.Sys.ServerVariables["Giphy"]["ApiKey"]
+            ? Umbraco.Sys.ServerVariables["Giphy"]["ApiKey"]
+            : $scope.model.config.apikey,
+        title: "Animated Gif Search",
+        subtitle: "Powered by GIPHY",
+        view: "/App_Plugins/GiphyPropertyEditorUmbraco7/giphy.picker.html",
+        show: false,
+        hideSubmitButton: true,
+        submit: function (model) {
+            $scope.control.value = model.images.original.url;
+            $scope.overlay.show = false;
+        },
+        close: function () {
+            $scope.overlay.show = false;
+        }
     };
 
     $scope.remove = function () {
--- GiphyPropertyEditorUmbraco7/App_Plugins/GiphyPropertyEditorUmbraco7/js/giphy.propertyeditor.controller.js
+++ GiphyPropertyEditorUmbraco7/App_Plugins/GiphyPropertyEditorUmbraco7/js/giphy.propertyeditor.controller.js
@@ -1,22 +1,25 @@
-﻿function giphyPropertyEditorController($scope, dialogService) {
+﻿function giphyPropertyEditorController($scope) {
+
+    $scope.overlay = {
+        apikey: Umbraco.Sys.ServerVariables["Giphy"]["ApiKey"]
+            ? Umbraco.Sys.ServerVariables["Giphy"]["ApiKey"]
+            : $scope.model.config.apikey,
+        title: "Animated Gif Search",
+        subtitle: "Powered by GIPHY",
+        view: "/App_Plugins/GiphyPropertyEditorUmbraco7/giphy.picker.html",
+        show: false,
+        hideSubmitButton: true,
+        submit: function (model) {
+            $scope.model.value = model.images.original.url;
+            $scope.overlay.show = false;
+        },
+        close: function () {
+            $scope.overlay.show = false;
+        }
+    };
 
     $scope.add = function () {
-        $scope.giphyPicker = {
-            apikey: Umbraco.Sys.ServerVariables["Giphy"]["ApiKey"]
-                ? Umbraco.Sys.ServerVariables["Giphy"]["ApiKey"]
-                : $scope.model.config.apikey,
-            title: "Animated Gif Search",
-            view: "/App_Plugins/GiphyPropertyEditorUmbraco7/giphy.picker.html",
-            show: true,
-            submit: function (model) {
-                $scope.model.value = model.images.original.url;
-                $scope.giphyPicker.show = false;
-            },
-            close: function () {
-                $scope.giphyPicker.show = false;
-                $scope.giphyPicker = null;
-            }
-        };
+        $scope.overlay.show = true;
     };
 
     $scope.remove = function () {
